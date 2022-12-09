074097d7bb1b40eaacb07d271755241babc5f96b
DevPacho <frankjose2004@gmail.com>
2022-12-08 22:39:06 -0500

    [FIXED] pycodestyle - too long line

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,7 +2,8 @@
 
 """
 You have n number of locked boxes in front of you.
-Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+Each box is numbered sequentially from 0 to n - 1 and
+each box may contain keys to the other boxes.
 
 Write a method that determines if all the boxes can be opened.
 
